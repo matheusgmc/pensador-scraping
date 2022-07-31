772a204b9c1d1a1f027e1a976eadce80c4cdec6c
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-31 07:53:41 -0400

    adicionando o tratamento para string inválidas

--- src/module/pensador.ts
+++ src/module/pensador.ts
@@ -1,14 +1,16 @@
 import { AxiosInstance } from "axios";
 import { IResponsePensador } from "../@types/pensador";
+import Utils from "../utils/";
 
 export class Pensador {
 	constructor(private router: AxiosInstance) {}
 
-	async search(q: string): Promise<IResponsePensador> {
+	async searchWord(q: string): Promise<IResponsePensador> {
 		try {
-			const { data } = await this.router.get(
-				"/busca.php?q=" + q.replace(" ", "+")
-			);
+			if (!q || Utils.checkString(q)) {
+				throw new Error("essa query não é válido.");
+			}
+			const { data } = await this.router.get("/" + q.replace(" ", "_"));
 			return { html: data };
 		} catch (error: any) {
 			return { err: error.message };
@@ -17,6 +19,9 @@ export class Pensador {
 
 	async getAuthor(author: string): Promise<IResponsePensador> {
 		try {
+			if (!author || Utils.checkString(author)) {
+				throw new Error("essa query não é válido.");
+			}
 			const { data } = await this.router(
 				"/autor/" + author.toLowerCase().replace(" ", "_")
 			);
