982d1f38b58cea864c30c53ceaaa0cd5d8f2d8b5
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-31 07:51:44 -0400

    alterando a tipagem de response

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,4 +1,19 @@
+import { Author } from "../entities/author";
+import { Thought } from "../entities/thought";
+
 export interface IPensador {
 	query: string;
 	limit?: number;
+}
+
+export interface IResponseSearch {
+	total: number;
+	query: string;
+	thought: Thought[];
+	author: Author;
+}
+
+export interface IResponse<T> {
+	sucess?: T;
+	error?: string;
 }
