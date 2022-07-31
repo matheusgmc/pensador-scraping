35f852b17a8458d5d6d162d6483c787ac82133db
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-31 07:51:11 -0400

    adicionando prop tags e bio

--- src/entities/author.ts
+++ src/entities/author.ts
@@ -3,11 +3,14 @@ export class Author {
 	public info: string;
 	public thought_total: number;
 	public associated: string[];
+	public tags?: string;
+	public bio?: string;
 	constructor(props: Author) {
 		this.associated = props.associated;
 		this.info = props.info;
 		this.name = props.name;
 		this.thought_total = props.thought_total;
+		this.bio = props.bio;
 		Object.freeze(this);
 	}
 }
