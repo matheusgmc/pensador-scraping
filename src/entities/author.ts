99e267e0946aa12dc31b89a7251abe612501d496
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-31 09:50:49 -0400

    adicionando imagem do autor

--- src/entities/author.ts
+++ src/entities/author.ts
@@ -1,5 +1,6 @@
 export class Author {
 	public name: string;
+	public avatar_url: string;
 	public info: string;
 	public thought_total: number;
 	public associated: string[];
@@ -11,6 +12,7 @@ export class Author {
 		this.name = props.name;
 		this.thought_total = props.thought_total;
 		this.bio = props.bio;
+		this.avatar_url = props.avatar_url;
 		Object.freeze(this);
 	}
 }
