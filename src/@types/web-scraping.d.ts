6de43c4d284b547205b05d5c32b812d75d07c1b6
matheusgmc <matheus.geraldo1@outlook.com>
2022-07-31 08:17:01 -0400

    tipagem do response para o bioAuthorsScrap

--- src/@types/web-scraping.d.ts
+++ src/@types/web-scraping.d.ts
@@ -1,4 +1,5 @@
 import { Author } from "../entities/author";
+import { BioAuthor } from "../entities/bio-author";
 import { RankAuthor } from "../entities/rank-author";
 import { Thought } from "../entities/thought";
 
@@ -10,3 +11,4 @@ export interface IResponseWebScrapingThought {
 export interface IResponseWebScrapingAuthor extends Author {}
 
 export interface IResponseWebScrapingRakingAuthors extends RankAuthor {}
+export interface IResponseWebScrapingBioAuthor extends BioAuthor {}
