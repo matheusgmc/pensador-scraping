5d4f6d6ff63093c315a1010ad51a0dac8a9fb86e
matheusgmc <matheus.geraldo1@outlook.com>
2022-08-01 10:52:50 -0400

    alterando tipagem

--- src/pescador-scraping.ts
+++ src/pescador-scraping.ts
@@ -1,30 +1,22 @@
 import { ScrapingInstance } from "./module/web-scraping";
 import { Pensador } from "./module/pensador";
 import fetch from "./services/fetch";
-import type {
-	PensadorScraping,
-	IResponse,
-	IResponseSearch,
-	IPensador,
-	IAuthorProps,
-	IBioAuthorProps,
-	IRankingAuthorsProps,
-	Scraping,
-} from "pescador-scraping-types";
 
-export class PensadorScrapingInstance implements PensadorScraping {
+export class PensadorScrapingInstance implements PensadorScraping.events {
 	private pensador = new Pensador(fetch);
 	private scraping = new ScrapingInstance();
 
 	/**
 	 *
-	 * @param {IPensador}
-	 * @returns {Promise<IResponse<IResponseSearch>>}
+	 * @param {PensadorScraping.IPensador}
+	 * @returns {Promise<PensadorScraping.IResponse<PensadorScraping.IResponseSearch>>}
 	 */
 	async search({
 		limit = 1,
 		query,
-	}: IPensador): Promise<IResponse<IResponseSearch>> {
+	}: PensadorScraping.IPensador): Promise<
+		PensadorScraping.IResponse<PensadorScraping.IResponseSearch>
+	> {
 		const { err, html } = await this.pensador.searchWord(query);
 		if (err) {
 			return { error: err };
@@ -47,7 +39,9 @@ export class PensadorScrapingInstance implements PensadorScraping {
 
 	async aboutAuthor({
 		query,
-	}: Omit<IPensador, "limit">): Promise<IResponse<IAuthorProps>> {
+	}: Omit<PensadorScraping.IPensador, "limit">): Promise<
+		PensadorScraping.IResponse<PensadorScraping.IAuthorProps>
+	> {
 		const { err, html } = await this.pensador.getAuthor(query);
 		if (err) {
 			return { error: err };
@@ -68,7 +62,9 @@ export class PensadorScrapingInstance implements PensadorScraping {
 
 	async bioAuthor({
 		query,
-	}: Omit<IPensador, "limit">): Promise<IResponse<IBioAuthorProps>> {
+	}: Omit<PensadorScraping.IPensador, "limit">): Promise<
+		PensadorScraping.IResponse<PensadorScraping.IBioAuthorProps>
+	> {
 		const { err, html } = await this.pensador.getBio(query);
 		if (err) {
 			return { error: err };
@@ -83,7 +79,9 @@ export class PensadorScrapingInstance implements PensadorScraping {
 		};
 	}
 
-	async rankingAuthors(): Promise<IResponse<IRankingAuthorsProps[]>> {
+	async rankingAuthors(): Promise<
+		PensadorScraping.IResponse<PensadorScraping.IRankingAuthorsProps[]>
+	> {
 		const { err, html } = await this.pensador.getHome();
 		if (err) {
 			throw new Error(err);
@@ -95,5 +93,73 @@ export class PensadorScrapingInstance implements PensadorScraping {
 		return {
 			sucess: result,
 		};
+	}
+}
+
+declare namespace PensadorScraping {
+	/* protected scraping: Scraping;
+	protected pensador: Pensador; */
+	export interface IAuthorProps {
+		name: string;
+		avatar_url: string;
+		info: string;
+		thought_total: number;
+		associated: string[];
+		tags?: string;
+		bio?: string;
+	}
+	export interface IPensador {
+		query: string;
+		limit?: number;
+	}
+	export interface IResponseSearch extends IResponseScrapSearch {
+		query: string;
+		author: IAuthorProps;
+	}
+	export interface IResponseScrapSearch {
+		total: number;
+		thought: IThoughtProps[];
+	}
+	export interface IThoughtProps {
+		author: string;
+		content: string;
+		url: string;
+		image_url?: string;
+	}
+
+	export interface IResponse<T> {
+		sucess?: T;
+		error?: string;
+	}
+
+	export interface IBioAuthorProps {
+		title: string;
+		name: string;
+		associated: string[];
+		content: IContentProps[];
+	}
+	export interface IContentProps {
+		paragraph: string;
+		content: string[];
+	}
+
+	export interface IRankingAuthorsProps {
+		name: string;
+		avatar_url: string;
+		href: string;
+		position: number;
+	}
+	export interface events {
+		search(data: IPensador): Promise<IResponse<IResponseSearch>>;
+
+		aboutAuthor(
+			data: Omit<IPensador, "limit">
+		): Promise<IResponse<IAuthorProps>>;
+
+		bioAuthor(
+			data: Omit<IPensador, "limit">
+		): Promise<IResponse<IBioAuthorProps>>;
+
+		rankingAuthors(): Promise<IResponse<IRankingAuthorsProps[]>>;
 	}
 }
