966a134e1e70295528dba3b3dfe47f33b6c59220
matheusgmc <matheus.geraldo1@outlook.com>
2022-08-02 19:28:39 -0400

    exportando topics-scrap

--- src/modules/scraping/index.ts
+++ src/modules/scraping/index.ts
@@ -2,5 +2,11 @@ import { authorScrap } from "./author-scrap";
 import { bioAuthorsScrap } from "./bio-author-scrap";
 import { rakingAuthorsScrap } from "./ranking-authors-scrap";
 import { searchScrap } from "./search-scrap";
-
-export { authorScrap, bioAuthorsScrap, rakingAuthorsScrap, searchScrap };
+import { topicsScrap } from "./topics-scrap";
+export {
+	authorScrap,
+	bioAuthorsScrap,
+	rakingAuthorsScrap,
+	searchScrap,
+	topicsScrap,
+};
