f56380c688920a3ddbc94183d07321f5de86f859
matheusgmc <sdmastergames@hotmail.com>
2022-08-05 12:04:09 -0400

    exportando o get-href

--- src/modules/pensador/index.ts
+++ src/modules/pensador/index.ts
@@ -2,5 +2,5 @@ import { search } from "./search";
 import { getAuthor } from "./get-author";
 import { getHome } from "./get-home";
 import { getBio } from "./get-bio-author";
-
-export { search, getAuthor, getHome, getBio };
+import { getHref } from "./get-href";
+export { search, getAuthor, getHome, getBio, getHref };
